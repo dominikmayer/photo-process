e60de17859f10dabb649cfb31ec0240a927d3af5
Dominik Mayer <dominik.mayer@gmail.com>
2017-01-08 01:41:21 +0100

    Added types.

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,11 +5,30 @@ import Data.Time.Format
 import Data.Time.LocalTime
 import Data.Map
 
+data Media = Photo { path :: FilePath
+                   , exifTime :: LocalTime
+                   --, fileTime :: LocalTime
+                   }
+           | LivePhoto { photoPath :: FilePath
+                       , videoPath :: FilePath
+                       , exifTime :: LocalTime
+                       , fileTime :: LocalTime
+                       }
+           | Image { path :: FilePath
+                   --, fileTime :: LocalTime
+                   }
+           | Video { path :: FilePath
+                   --, fileTime :: LocalTime
+                   } deriving (Show)
+
 photoProcess :: [FilePath] -> IO ()
-photoProcess arguments = do
-  dateStrings <- getDateStringFromFileList arguments
+photoProcess paths = do
+  dateStrings <- getDateStringFromFileList paths
   putStrLn "I found images from the following times:"
   mapM_ putStrLn dateStrings
+  putStrLn "Media:"
+  media <- mapM classifyFile paths
+  mapM_ print media
 
 getDateStringFromFileList :: [FilePath] -> IO [String]
 getDateStringFromFileList = mapM getDateStringFromFile
@@ -17,13 +36,32 @@ getDateStringFromFileList = mapM getDateStringFromFile
 getDateStringFromFile :: FilePath -> IO String
 getDateStringFromFile filePath = do
   exifData <- parseFileExif filePath
-  return (exifToString exifData)
+  return (exifToTimeString exifData)
 
-exifToString :: Either String (Map ExifTag ExifValue) -> String
-exifToString (Left message) = message
-exifToString (Right exifMap) = timeToString exifMap
+exifToTimeString :: Either String (Map ExifTag ExifValue) -> String
+exifToTimeString (Left message) = message
+exifToTimeString (Right exifMap) = timeToString exifMap
 
 timeToString :: Map ExifTag ExifValue -> String
 timeToString exifMap = case getDateTimeOriginal exifMap of
   Just localTime -> formatTime defaultTimeLocale "%Y-%m-%d - %H-%M-%S" localTime
   Nothing -> "No time available."
+
+getExifTime :: FilePath -> IO (Maybe LocalTime)
+getExifTime filePath = do
+  exifData <- parseFileExif filePath
+  return (exifToLocalTime exifData)
+
+exifToLocalTime :: Either String (Map ExifTag ExifValue) -> Maybe LocalTime
+exifToLocalTime (Left message) = Nothing
+exifToLocalTime (Right exifMap) = getDateTimeOriginal exifMap
+
+classifyFile :: FilePath -> IO Media
+classifyFile filePath = do
+  localTime <- getExifTime filePath
+  case localTime of
+    Just time -> return Photo { path = filePath
+                               , exifTime = time
+                               }
+    Nothing -> return Image { path = filePath
+                            }
